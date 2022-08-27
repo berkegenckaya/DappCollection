750792dd65a63ec971d722ef0efe6cdd0411e206
berkegenckaya <berkegenckaya@gmail.com>
2022-08-27 17:59:02 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ethers } from "ethers";
+import { useState, useEffect } from "react";
+import { SiWeb3Dotjs } from "react-icons/si";
+import abi from "./abi/abi.json";
+import data from "./data/data.json";
 
 function App() {
+  const [account, setAccount] = useState("");
+  const [provider, setProvider] = useState(null);
+  const [nfts, setNfts] = useState(data);
+
+  const balance = async () => {
+    const contract = new ethers.Contract(
+      "0xa27D1cEDF3AeCB7c88358caAaF4A27301e1F1a43",
+      abi,
+      provider
+    );
+    const tempBalance = await contract.balanceOf(
+      "0x8D8dB741CC92ea91F4ca70E3cF83148CCC682C34"
+    );
+  };
+
+  const initConnection = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const tempProvider = new ethers.providers.Web3Provider(window.ethereum);
+      setProvider(tempProvider);
+      setAccount(accounts[0]);
+    } else console.log("Please install metamask");
+  };
+
+  useEffect(() => {
+    initConnection();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="page">
+      <div className="header">
+        <img
+          src={require("./assets/images/ethereum.png")}
+          className="ethIcon"
+        />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Berke.Web3
+          <span>
+            <SiWeb3Dotjs style={{ marginLeft: "5px" }} />
+          </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {account == "" ? (
+          <button onClick={initConnection} className="button">
+            Connect
+          </button>
+        ) : (
+          <p>...{account.substring(account.length - 7)}</p>
+        )}
+      </div>
+      <div className="main">
+        {nfts.lists.map((nft, index) => {
+          return (
+            <div key={index}>
+              <p>{nft.name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
